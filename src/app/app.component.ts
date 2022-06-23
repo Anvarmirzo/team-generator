dee4186390f7fecc11f924e0649787ffb463b371
Anvar Akhmadjanov <www.latvienhoasadrizeges@gmail.com>
2022-06-23 16:24:32 +0500

    added reactive generate component and list of teams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,17 @@ import {FormBuilder, Validators,} from "@angular/forms";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  members: string[] = [];
+  members: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  teams: string[][] = [];
+  isValidTeamCount = true;
 
   teamForm = this.fb.group({
     newMemberName: ['', Validators.required],
   })
 
+  generateForm = this.fb.group({
+    teamCount: ['', Validators.required],
+  })
 
   constructor(private fb: FormBuilder) {
   }
@@ -23,6 +28,30 @@ export class AppComponent {
     if (trimmed) {
       this.members.push(trimmed)
       this.teamForm.reset();
+    }
+  }
+
+  generateTeams(teamCount?: string | null) {
+    if (teamCount) {
+      const count = +teamCount;
+      if (count > 0 && count <= this.members.length) {
+        this.isValidTeamCount = true;
+        this.teams = [];
+        const members = [...this.members];
+
+        while (members.length) {
+          for (let i = 0; i < count; i++) {
+            const randomIndex = Math.floor(Math.random() * members.length);
+            const member = members.splice(randomIndex, 1)[0];
+            if (!member) break;
+            this.teams[i] ? this.teams[i].push(member) : this.teams[i] = [member];
+          }
+        }
+
+        this.generateForm.reset();
+      } else {
+        this.isValidTeamCount = false;
+      }
     }
   }
 }
